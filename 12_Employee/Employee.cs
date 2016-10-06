0a1fa3ae04151429c08aa7b93f2f53b6cd54d1c5
Pelle <Pelle@LAPTOP-T69PBCOE>
2016-10-06 10:50:51 +0200

    Add project files.

---
+++ 12_Employee/Employee.cs
@@ -0,0 +1,22 @@
+﻿namespace _12_Employee
+{
+    internal class Employee
+    {
+        public int Id;
+        public string Name;
+        public string Type;
+
+        public Employee()
+        {
+
+        }
+        public Employee(string name, string ocupation, int id)
+        {
+            Name = name;
+            Type = ocupation;
+            Id = id;
+        }
+
+      
+    }
+}
---
+++ 12_Employee/EmployeeRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _12_Employee
+{
+    internal class EmployeeRepository
+    {   int _id                     = 1;
+        List<Employee> employeelist = new List<Employee>();
+        internal int NextID()
+        {
+           return _id++;
+        }
+        internal void Clear()
+        {
+            employeelist.Clear();
+        }
+
+        internal int CountEmployees()
+        {
+            return employeelist.Count();
+        }
+
+        internal Employee CreateEmployee(string name, string type)
+        {
+           
+            Employee _employee = new Employee(name, type, NextID());
+            employeelist.Add(_employee);
+            return _employee;
+        }
+
+        internal void SaveEmployee(Employee employee)
+        {
+           employeelist[employee.Id -1] = employee;
+        }
+
+        internal Employee LoadEmployee(int id)
+        {
+            return employeelist[id-1];
+        }
+
+        internal List<Employee> FindAllEmployees()
+        {
+            return employeelist;
+        }
+    }
+}
